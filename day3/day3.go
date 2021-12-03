b98e1c63eb69d5f7d41ae29f0f4cb62065df203e
sbennett-ihasco <simon.bennett@ihasco.co.uk>
2021-12-03 20:55:19 +0000

    Day 3 part 1

--- day3/day3.go
+++ day3/day3.go
@@ -3,11 +3,46 @@ package main
 import (
 	"fmt"
 	"sbennett-ihasco/aoc2021/common"
+	"sort"
+	"strconv"
+	"strings"
 )
 
-func p1(values []string) int {
-	x := 0
-	return x
+func p1(values []string) int64 {
+
+	ones := make(map[int]int)
+	var gamma string
+	var epsilon string
+	threshold := len(values) / 2
+
+	for _, value := range values {
+		parts := strings.Split(value, "")
+		for i, bit := range parts {
+			if bit != "0" {
+				ones[i]++
+			}
+		}
+	}
+
+	keys := make([]int, 0)
+	for i, _ := range ones {
+		keys = append(keys, i)
+	}
+	sort.Ints(keys)
+	for _, i := range keys {
+		if ones[i] > threshold {
+			gamma += "1"
+			epsilon += "0"
+		} else {
+			gamma += "0"
+			epsilon += "1"
+		}
+	}
+
+	ret1, _ := strconv.ParseInt(gamma, 2, 64)
+	ret2, _ := strconv.ParseInt(epsilon, 2, 64)
+
+	return ret1 * ret2
 }
 
 func p2(values []string) int {
