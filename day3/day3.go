e4be0abd5d4be9823a90f5b2d8443c8b13a1009f
sbennett-ihasco <simon.bennett@ihasco.co.uk>
2021-12-04 14:10:32 +0000

    Some refactoring

--- day3/day3.go
+++ day3/day3.go
@@ -6,16 +6,16 @@ import (
 	"strconv"
 )
 
-var gLineLength int
+var bits int
 
 func main() {
 	values := common.ReadStrings("day3.values")
 
-	gLineLength = len(values[0])
+	bits = len(values[0])
 
 	integers := make([]int, len(values))
-	for i, line := range values {
-		converted, _ := strconv.ParseInt(line, 2, 64)
+	for i, value := range values {
+		converted, _ := strconv.ParseInt(value, 2, 64)
 		integers[i] = int(converted)
 	}
 
@@ -25,58 +25,64 @@ func main() {
 }
 
 func part1(integers []int) int {
-	ret1 := mostOrLeastCommon(integers, true)
-	ret2 := mostOrLeastCommon(integers, false)
-	return ret1 * ret2
+	most := mostOrLeastCommon(integers, true)
+	least := mostOrLeastCommon(integers, false)
+	return most * least
 }
 
 func part2(integers []int) int {
-	ret1 := reduceByMostMatches(integers, true)
-	ret2 := reduceByMostMatches(integers, false)
-	return ret1 * ret2
+	most := reduceByMostOrLeastCommon(integers, true)
+	least := reduceByMostOrLeastCommon(integers, false)
+	return most * least
 }
 
-func mostOrLeastCommon(integers []int, matchMost bool) int {
-	columnCounts := make([]int, gLineLength)
-	for column := 0; column < gLineLength; column++ {
-		bitMask := 1 << (gLineLength - column - 1)
+func mostOrLeastCommon(integers []int, mostCommon bool) int {
+	columnCounts := make([]int, bits)
+
+	for bit := 0; bit < bits; bit++ {
+		mask := 1 << (bits - bit - 1)
 		for _, integer := range integers {
-			if integer&bitMask > 0 {
-				columnCounts[column]++
+			if integer&mask == 0 {
+				columnCounts[bit]--
 			} else {
-				columnCounts[column]--
+				columnCounts[bit]++
 			}
 		}
 	}
 
 	match := 0
-	for column, columnCount := range columnCounts {
-		bitMask := 1 << (gLineLength - column - 1)
-		if (columnCount >= 0) == matchMost {
-			match |= bitMask
+	for bit, columnCount := range columnCounts {
+		mask := 1 << (bits - bit - 1)
+		if (columnCount >= 0) == mostCommon {
+			match |= mask
 		}
 	}
+
 	return match
 }
 
-func reduceByMostMatches(integers []int, matchMost bool) int {
-	for i := 0; i < gLineLength; i++ {
-		t := mostOrLeastCommon(integers, matchMost)
-		integers = sift(integers, t, i)
+func reduceByMostOrLeastCommon(integers []int, mostCommon bool) int {
+	for column := 0; column < bits; column++ {
+		matches := mostOrLeastCommon(integers, mostCommon)
+		integers = reduce(integers, matches, column)
 		if len(integers) == 1 {
 			break
 		}
 	}
-	return integers[0]
+
+	return integers[0] // one remains
 }
 
-func sift(integers []int, match int, position int) []int {
-	var sifted []int
-	bitMask := 1 << (gLineLength - position - 1)
+func reduce(integers []int, match int, position int) []int {
+	var reduced []int
+
+	mask := 1 << (bits - position - 1)
+
 	for _, integer := range integers {
-		if (integer&bitMask == 0) == (match&bitMask == 0) {
-			sifted = append(sifted, integer)
+		if (integer&mask == 0) == (match&mask == 0) {
+			reduced = append(reduced, integer)
 		}
 	}
-	return sifted
+
+	return reduced
 }
