a49f3891afb4299f7fe14ce2ee0ec7f4f21d5bd5
sbennett-ihasco <simon.bennett@ihasco.co.uk>
2021-12-04 13:18:32 +0000

    Hard work

--- day3/day3.go
+++ day3/day3.go
@@ -3,57 +3,80 @@ package main
 import (
 	"fmt"
 	"sbennett-ihasco/aoc2021/common"
-	"sort"
 	"strconv"
-	"strings"
 )
 
-func p1(values []string) int64 {
+var gLineLength int
 
-	ones := make(map[int]int)
-	var gamma string
-	var epsilon string
-	threshold := len(values) / 2
+func main() {
+	values := common.ReadStrings("day3.values")
 
-	for _, value := range values {
-		parts := strings.Split(value, "")
-		for i, bit := range parts {
-			if bit == "1" {
-				ones[i]++
+	gLineLength = len(values[0])
+
+	integers := make([]int, len(values))
+	for i, line := range values {
+		converted, _ := strconv.ParseInt(line, 2, 64)
+		integers[i] = int(converted)
+	}
+
+	fmt.Println("https://adventofcode.com/2021/day/3")
+	fmt.Printf("Day 3, part 1: %v\n", part1(integers))
+	fmt.Printf("Day 3, part 2: %v\n", part2(integers))
+}
+
+func part1(integers []int) int {
+	ret1 := mostOrLeastCommon(integers, true)
+	ret2 := mostOrLeastCommon(integers, false)
+	return ret1 * ret2
+}
+
+func part2(integers []int) int {
+	ret1 := reduceByMostMatches(integers, true)
+	ret2 := reduceByMostMatches(integers, false)
+	return ret1 * ret2
+}
+
+func mostOrLeastCommon(integers []int, matchMost bool) int {
+	columnCounts := make([]int, gLineLength)
+	for column := 0; column < gLineLength; column++ {
+		bitMask := 1 << (gLineLength - column - 1)
+		for _, integer := range integers {
+			if integer&bitMask > 0 {
+				columnCounts[column]++
+			} else {
+				columnCounts[column]--
 			}
 		}
 	}
 
-	keys := make([]int, 0)
-	for i, _ := range ones {
-		keys = append(keys, i)
-	}
-	sort.Ints(keys)
-	for _, i := range keys {
-		if ones[i] > threshold {
-			gamma += "1"
-			epsilon += "0"
-		} else {
-			gamma += "0"
-			epsilon += "1"
+	match := 0
+	for column, columnCount := range columnCounts {
+		bitMask := 1 << (gLineLength - column - 1)
+		if (columnCount >= 0) == matchMost {
+			match |= bitMask
 		}
 	}
-
-	ret1, _ := strconv.ParseInt(gamma, 2, 64)
-	ret2, _ := strconv.ParseInt(epsilon, 2, 64)
-
-	return ret1 * ret2
+	return match
 }
 
-func p2(values []string) int {
-	x := 0
-	return x
+func reduceByMostMatches(integers []int, matchMost bool) int {
+	for i := 0; i < gLineLength; i++ {
+		t := mostOrLeastCommon(integers, matchMost)
+		integers = sift(integers, t, i)
+		if len(integers) == 1 {
+			break
+		}
+	}
+	return integers[0]
 }
 
-func main() {
-	lines := common.ReadStrings("day3.values")
-
-	fmt.Println("https://adventofcode.com/2021/day/3")
-	fmt.Printf("Day 3, part 1: %v\n", p1(lines))
-	fmt.Printf("Day 3, part 2: %v\n", p2(lines))
+func sift(integers []int, match int, position int) []int {
+	var sifted []int
+	bitMask := 1 << (gLineLength - position - 1)
+	for _, integer := range integers {
+		if (integer&bitMask == 0) == (match&bitMask == 0) {
+			sifted = append(sifted, integer)
+		}
+	}
+	return sifted
 }
