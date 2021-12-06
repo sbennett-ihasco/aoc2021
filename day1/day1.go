3b61b4389ac137f357175d79c4d02dab1d69d263
sbennett-ihasco <simon.bennett@ihasco.co.uk>
2021-12-06 13:53:32 +0000

    Use named/naked return

--- day1/day1.go
+++ day1/day1.go
@@ -5,22 +5,17 @@ import (
 	"sbennett-ihasco/aoc2021/common"
 )
 
-func calculateIncrements(values []int, offset int) int {
-	increments := 0
-
-	for i, current := range values[offset:] {
-		if current > values[i] {
+func calculateIncrements(depths []int, offset int) (increments int) {
+	for i, current := range depths[offset:] {
+		if current > depths[i] {
 			increments++
 		}
 	}
-
-	return increments
+	return
 }
 
 func main() {
-	lines := common.ReadIntegers("day1.values")
-
-	fmt.Println("https://adventofcode.com/2021/day/1")
-	fmt.Printf("Day 1, part 1: %v\n", calculateIncrements(lines, 1))
-	fmt.Printf("Day 1, part 2: %v\n", calculateIncrements(lines, 3))
+	input := common.ReadIntegers("day1.values")
+	fmt.Println(calculateIncrements(input, 1))
+	fmt.Println(calculateIncrements(input, 3))
 }
